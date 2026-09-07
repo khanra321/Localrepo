75bbc55164221840333ac34f6a31b46a186ca5cc
Khanra321 <akashkhanra3063@gmail.com>
2026-09-08 00:18:25 +0530

    by indexsing:

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let show= document.getElementById("demo");
-show.innerHTML = "Hello world"
+let show = document.getElementById("demo");
+const show1 = document.getElementsByClassName("demo1");
+const x = document.getElementById("main");
+const y = x.getElementsByTagName("p");
+show.innerHTML = "Hello world";
+show1[0].innerHTML = "The first paragraph (index 0) inside 'main' is: " + y[0].innerHTML;
+
